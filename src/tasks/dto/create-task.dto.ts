50b7302a19e9f683ffbce5f3abf325370fec4fd4
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-07-01 12:06:56 +0300

    search and filter

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, Length, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @Length(5, 50)
   title: string;
+  @IsNotEmpty()
+  @MinLength(50)
   description: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,18 +6,25 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getAllTasks(@Query() filter: GetTasksFilterDto): Task[] {
+    // console.log(filter);
+    if (Object.keys(filter).length) {
+      return this.tasksService.getTasksWithFilter(filter);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
