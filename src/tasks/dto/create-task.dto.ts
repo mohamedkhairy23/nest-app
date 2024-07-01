1410fc0fea87c526cbdea383c24a990d82786c38
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-07-01 12:13:27 +0300

    applying validation pipe with dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,9 +1,9 @@
 import { IsNotEmpty, Length, MinLength } from 'class-validator';
 
 export class CreateTaskDto {
-  @Length(5, 50)
+  @Length(5, 50) //
   title: string;
-  @IsNotEmpty()
-  @MinLength(50)
+  @IsNotEmpty() //
+  @MinLength(50) //
   description: string;
 }
